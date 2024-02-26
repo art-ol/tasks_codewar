79ef0da73ecc13810cce931d8727c637141a3014
Artyom Oleinik <art.oleinik.dev@gmail.com>
2024-02-26 13:16:57 +0530

    2102 1st task

---
+++ 7Q210224/Stop_gninnipS_My_sdroW!.js
@@ -0,0 +1,7 @@
+function spinWords(string){
+  //TODO Have fun :)
+  return string.split(' ')
+    .map(word =>
+      (word.length > 4 ? word.split('').reverse().join('') : word))
+    .join(' ');
+}
