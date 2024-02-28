31c552cf1abe6cd4e659e28dff5332900f29f6f2
Artyom Oleinik <art.oleinik.dev@gmail.com>
2024-02-28 17:07:20 +0530

    solve 2nd task

---
+++ 7Q210224/FindTheOddInt.js
@@ -0,0 +1,18 @@
+// function findOdd(arr) {
+//   //happy coding!
+//   const objOfNumbersAndTheirCount = arr.reduce((acc, number) => {
+//     acc[number] = (acc[number] || 0) + 1;
+//     return acc;
+//   },{});
+
+//   for (const [key, value] of Object.entries(objOfNumbersAndTheirCount)) {
+//     if (value % 2 !== 0) return Number(key);
+//   }
+// }
+
+// используя reduce, аккумулируем число с помощью XOR, 
+// в итоге все числа, встречающиеся четное кол-во раз дадут 0,
+// а единственное число, встречающееся нечетное кол-во раз даст само себя
+const findOdd = (arr) => arr.reduce((acc, number) => acc ^ number);
+
+console.log(findOdd([1,2,2,3,3,3,4,3,3,3,2,2,1,4,2,3]));
